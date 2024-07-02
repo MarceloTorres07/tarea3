a38ef8e5453257003cee556e9a805caac3bb05de
Marcelo Torres <marceloignacio.torres@alumnos.ulagos.cl>
2024-07-02 00:27:37 -0400

    tarea 3 version 1.0.0

---
+++ index.js
@@ -0,0 +1,18 @@
+let c1='fuerte'; //fuerte, pequeño, travieso
+let c2='veloz'; //comilon, veloz, debil, jugeton
+
+if(c1=== 'fuerte' && c2=== 'comilon'){
+    personaje = ' goku';
+    console.log('eres' + personaje);
+} else if (c1=== 'fuerte' && c2=== 'veloz'){
+    personaje = ' vegeta';
+    console.log('eres' + personaje);
+}else if (c1=== 'pequeño' && c2=== 'debil'){
+    personaje = ' krilin';
+    console.log('eres' + personaje);
+}else if (c1=== 'travieso' && c2=== 'jugeton'){
+    personaje = ' trunks';
+    console.log('eres' + personaje);
+}else{
+    console.log('no estudiaste nada');
+}
